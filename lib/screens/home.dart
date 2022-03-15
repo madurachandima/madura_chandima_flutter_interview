31629d39274c9319381cacbb0a1d5d66845fe0fd
madurachandima <40019782+madurachandima@users.noreply.github.com>
2022-03-15 17:29:55 +0530

    init

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:madura_chandima_flutter_interview/widgets/custome_appbar.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomeAppbar(),
-    );
+    return Scaffold();
   }
 }
